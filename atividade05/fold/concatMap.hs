0b2dce7fe349e2c1f279357e13697617ab6825a5
luizenrik <84132822+luizenrike@users.noreply.github.com>
2021-06-24 10:49:25 -0300

    Update concatMap.hs

--- atividade05/fold/concatMap.hs
+++ atividade05/fold/concatMap.hs
@@ -1,6 +1,6 @@
 concatmap fn xs = foldl (++) [] (map fn xs)
 
 main = do
-    print $ concatmap (\x -> [x,x]) [1,2,3,4] -- [1,1,2,2,3,3,4,4]
-    print $ concatMap (enumFromTo 1) [1,3,5] -- [1,1,2,3,1,2,3,4,5]
+    print $ concatmap (\x -> [x,x]) [1,2,3,4] 
+    print $ concatMap (enumFromTo 1) [1,3,5] 
     print $ concatMap (\x -> [(x,x+2,x/2)]) [1,3,5]
